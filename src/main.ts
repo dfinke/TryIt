8199f07a93806e9732f96f20afcefb9d8da57f33
dfinke <finked@hotmail.com>
2020-01-01 16:09:05 -0500

    Hello World

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,32 @@ import * as fs from 'fs'
 import { octodex_url, octocats } from './octocats'
 
 async function run() {
-    try {
-        const rand = Math.floor(Math.random() * octocats.length)
-        console.log("Hello World")
-        // const token = process.env.GITHUB_TOKEN || ''
+    // try {
+    const rand = Math.floor(Math.random() * octocats.length)
+    console.log("Hello World")
+    // const token = process.env.GITHUB_TOKEN || ''
 
-        // const context = (github as any).context
-        // const command = context.payload.comment.body.trim()
+    // const context = (github as any).context
+    // const command = context.payload.comment.body.trim()
 
-        // if (command == '/octocat' || command == '/mona') {
-        //     const octokit = new github.GitHub(token)
-        //     const repository = context.payload.repository
-        //     const issue = context.payload.issue
+    // if (command == '/octocat' || command == '/mona') {
+    //     const octokit = new github.GitHub(token)
+    //     const repository = context.payload.repository
+    //     const issue = context.payload.issue
 
-        //     const rand = Math.floor(Math.random() * octocats.length)
-        //     const octocat = octocats[rand]
+    //     const rand = Math.floor(Math.random() * octocats.length)
+    //     const octocat = octocats[rand]
 
-        //     octokit.issues.createComment({
-        //         owner: repository.owner.login,
-        //         repo: repository.name,
-        //         issue_number: issue.number,
-        //         body: '![' + octocat + '](' + octodex_url + '/' + octocat + ')'
-        //     })
-    }
-    } catch (err) {
-    core.setFailed(err.message)
-}
+    //     octokit.issues.createComment({
+    //         owner: repository.owner.login,
+    //         repo: repository.name,
+    //         issue_number: issue.number,
+    //         body: '![' + octocat + '](' + octodex_url + '/' + octocat + ')'
+    //     })
+    // }
+    //     } catch (err) {
+    //     core.setFailed(err.message)
+    // }
 }
 
 run();
