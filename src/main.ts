72fb68c8d16d571ef6456db54dbe1873449992cf
dfinke <finked@hotmail.com>
2020-01-01 14:58:17 -0500

    print to the console

--- src/main.ts
+++ src/main.ts
@@ -7,29 +7,31 @@ import { octodex_url, octocats } from './octocats'
 
 async function run() {
     try {
-        const token = process.env.GITHUB_TOKEN || ''
+        const rand = Math.floor(Math.random() * octocats.length)
+        console.log(rand)
+        // const token = process.env.GITHUB_TOKEN || ''
 
-        const context = (github as any).context
-        const command = context.payload.comment.body.trim()
+        // const context = (github as any).context
+        // const command = context.payload.comment.body.trim()
 
-        if (command == '/octocat' || command == '/mona') {
-            const octokit = new github.GitHub(token)
-            const repository = context.payload.repository
-            const issue = context.payload.issue
+        // if (command == '/octocat' || command == '/mona') {
+        //     const octokit = new github.GitHub(token)
+        //     const repository = context.payload.repository
+        //     const issue = context.payload.issue
 
-            const rand = Math.floor(Math.random() * octocats.length)
-            const octocat = octocats[rand]
+        //     const rand = Math.floor(Math.random() * octocats.length)
+        //     const octocat = octocats[rand]
 
-            octokit.issues.createComment({
-                owner: repository.owner.login,
-                repo: repository.name,
-                issue_number: issue.number,
-                body: '![' + octocat + '](' + octodex_url + '/' + octocat + ')'
-            })
-        }
-    } catch (err) {
-        core.setFailed(err.message)
+        //     octokit.issues.createComment({
+        //         owner: repository.owner.login,
+        //         repo: repository.name,
+        //         issue_number: issue.number,
+        //         body: '![' + octocat + '](' + octodex_url + '/' + octocat + ')'
+        //     })
     }
+    } catch (err) {
+    core.setFailed(err.message)
+}
 }
 
 run();
